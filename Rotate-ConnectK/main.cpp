634b04f7b70fd48cab0ed1b2f3b1343ed9ad82c3
Akhil Verghese <akhil@Akhils-MacBook-Pro.local>
2013-03-12 12:24:44 +0530

    Initial Commit

---
+++ Rotate-ConnectK/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Rotate-ConnectK
+//
+//  Created by Akhil Verghese on 3/12/13.
+//  Copyright (c) 2013 Akhil Verghese. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
